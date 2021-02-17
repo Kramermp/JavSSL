8866e89730d11dd20a05638a42972f334478ccef
Kramer <kramermp5@gmail.com>
2021-02-17 18:19:25 -0500

    Changed to socket connection to support saving bad certs

--- src/main/java/javssl/sclient/S_Client.java
+++ src/main/java/javssl/sclient/S_Client.java
@@ -5,12 +5,15 @@ import javssl.util.BadArgumentsException;
 import sun.misc.BASE64Encoder;
 import sun.security.provider.X509Factory;
 
-import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.*;
 import java.io.*;
 import java.net.MalformedURLException;
+import java.net.SocketException;
 import java.net.URL;
+import java.security.*;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateEncodingException;
+import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 
 public class S_Client {
@@ -18,6 +21,9 @@ public class S_Client {
 	String target = "";
 	URL targetURL = null;
 	HttpsURLConnection con = null;
+
+	SSLSocket socket;
+	X509Certificate[] chain;
 
 	Certificate[] certs = null;
 
@@ -37,25 +43,7 @@ public class S_Client {
 		System.out.println("Running S_Client");
 		System.out.println("Target: " + target);
 
-		try {
-			con = (HttpsURLConnection) targetURL.openConnection();
-			con.getInputStream();
-			certs = con.getServerCertificates();
-
-			if(showCertsFlag) {
-				printCerts();
-			}
-
-			if(saveCertFlag) {
-				saveCert();
-			}
-
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		System.out.println("Connected Successfully");
+		socketConnection();
 	}
 
 	private void processArgs() throws BadArgumentsException, MalformedURLException {
@@ -87,14 +75,7 @@ public class S_Client {
 		}
 	}
 
-	private void printCerts() {
-		for(int i =0; i < certs.length; i++) {
-			System.out.println(certs[i]);
-		}
-	}
-
-	private void saveCert() {
-		X509Certificate xCert = (X509Certificate) certs[0];
+	private void saveCert(X509Certificate xCert) {
 		try {
 			BASE64Encoder encoder = new BASE64Encoder();
 			PrintWriter out = new PrintWriter(new File(fileLocation));
@@ -111,4 +92,135 @@ public class S_Client {
 		}
 	}
 
+	private void socketConnection() {
+		SSLContext context = null;
+		try {
+
+			File file = new File("jssecacerts");
+			if (file.isFile() == false) {
+				char SEP = File.separatorChar;
+				File dir = new File(System.getProperty("java.home") + SEP
+						+ "lib" + SEP + "security");
+				file = new File(dir, "jssecacerts");
+				if (file.isFile() == false) {
+					file = new File(dir, "cacerts");
+				}
+			}
+
+			char[] passphrase = "changeit".toCharArray();
+			System.out.println("Loading KeyStore " + file + "...");
+			InputStream in = new FileInputStream(file);
+			KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+			ks.load(in, passphrase);
+			in.close();
+
+			context = SSLContext.getInstance("TLS");
+			TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+			tmf.init(ks);
+			X509TrustManager defaultTrustManager = (X509TrustManager)tmf.getTrustManagers()[0];
+			SavingTrustManager tm = new SavingTrustManager(defaultTrustManager);
+			context.init(null, new TrustManager[] {tm}, null);
+			SSLSocketFactory factory = context.getSocketFactory();
+
+			System.out.println("Opening connection to " + targetURL.getHost() + ":" + 443 + "...");
+			socket = (SSLSocket)factory.createSocket(targetURL.getHost(), 443);
+			socket.setSoTimeout(10000);
+			try {
+				System.out.println("Starting SSL handshake...");
+				socket.startHandshake();
+				socket.close();
+				System.out.println();
+				System.out.println("No errors, certificate is already trusted");
+			} catch (SSLException e) {
+				System.out.println("SSL Exception");
+				e.printStackTrace(System.out);
+				System.out.println("Exception During Handshake");
+			}
+
+			chain = tm.chain;
+			if (chain == null) {
+				System.out.println("Could not obtain server certificate chain");
+				return;
+			}
+
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println();
+			System.out.println("Server sent " + chain.length + " certificate(s):");
+			System.out.println();
+			if(showCertsFlag) {
+				MessageDigest sha1 = MessageDigest.getInstance("SHA1");
+				MessageDigest md5 = MessageDigest.getInstance("MD5");
+				for (int i = 0; i < chain.length; i++) {
+					X509Certificate cert = chain[i];
+					System.out.println
+							(" " + (i + 1) + " Subject " + cert.getSubjectDN());
+					System.out.println("   Issuer  " + cert.getIssuerDN());
+					sha1.update(cert.getEncoded());
+					System.out.println("   sha1    " + toHexString(sha1.digest()));
+					md5.update(cert.getEncoded());
+					System.out.println("   md5     " + toHexString(md5.digest()));
+					System.out.println();
+				}
+			}
+
+			if(saveCertFlag) {
+				saveCert(chain[0]);
+			}
+
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (SocketException e) {
+			System.out.println("Socket Exception");
+			e.printStackTrace();
+		} catch (KeyManagementException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("IO Exception");
+			e.printStackTrace();
+		} catch (CertificateException e) {
+			System.out.println("Cert Exception");
+		} catch (KeyStoreException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private static final char[] HEXDIGITS = "0123456789abcdef".toCharArray();
+
+	private static String toHexString(byte[] bytes) {
+		StringBuilder sb = new StringBuilder(bytes.length * 3);
+		for (int b : bytes) {
+			b &= 0xff;
+			sb.append(HEXDIGITS[b >> 4]);
+			sb.append(HEXDIGITS[b & 15]);
+			sb.append(' ');
+		}
+		return sb.toString();
+	}
+
+	private static class SavingTrustManager implements X509TrustManager {
+
+		private final X509TrustManager tm;
+		private X509Certificate[] chain;
+
+		SavingTrustManager(X509TrustManager tm) {
+			this.tm = tm;
+		}
+
+		public X509Certificate[] getAcceptedIssuers() {
+			throw new UnsupportedOperationException();
+		}
+
+		public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+				throw new UnsupportedOperationException();
+		}
+
+		public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+			this.chain = chain;
+			tm.checkServerTrusted(chain, authType);
+		}
+	}
 }
+
